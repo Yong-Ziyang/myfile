74c8c0a582e2d902c78048ba78153acc513d20b8
yongziyang <1051785839@qq.com>
2017-12-24 22:14:16 +0800

    this is my file

---
+++ TENSOR_FLOW/tensorflow_svm.py
@@ -0,0 +1,9 @@
+# usr/bin/env python3
+# -*-coding:UTF-8-*-
+
+# 基于Tensorflow的支持向量机
+import tensorflow as tf
+import numpy as np
+from sklearn import  datasets
+import matplotlib as plt
+iris=datasets.load_iris()
